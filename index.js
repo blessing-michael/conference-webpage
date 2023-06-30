f7f60cf588b661684d62348c7f1d0b04c845b9bd
blessing-michael <blessingmike334@gmail.com>
2023-06-30 15:08:58 -0700

    Add javacript functionality in index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const speakers = [{
   id: 1,
   title: 'Didy Mike',
+  img: './images/black-1.webp',
   backgound: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
   desc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
 },
 {
   id: 2,
   title: 'Didy Mike',
+  img: './images/black-2.jpeg',
   backgound: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
   desc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
 },
 {
   id: 3,
   title: 'Didy Mike',
+  img: './images/black-3.webp',
   backgound: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
   desc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
 },
 {
   id: 4,
   title: 'Didy Mike',
+  img: './images/black-3.webp',
   backgound: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
   desc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
 },
@@ -37,7 +41,7 @@ function generateWork() {
    
     <div class="speakers-container">
         <div class="speaker-one">
-            <img src="./images/person3.jpeg" alt="speaker" class="speaker-img">
+            <img src=${work.img} alt="speaker" class="speaker-img">
             <div class="speaker-info">
                 <h4>${work.title}</h4>
                 <p class="info-one">${work.backgound}</p>
